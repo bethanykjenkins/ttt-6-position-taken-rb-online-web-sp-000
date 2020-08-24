71e2667f303dadfb74f376a84d36a2a25c0cc603
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2020-08-24 23:40:49 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 def position_taken? (board, index)
   if board[index] == " " || board[index] == ""
    taken = false
-  else 
-    taken = nil
+  if board[index] == nil 
+    taken = false
   end
   
 end
